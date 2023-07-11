bac01ba467fdb50f6d7ecd21301dea5232fedf46
Quintanaaalberto <quintanaaalberto@gmail.com>
2023-07-11 19:18:11 +0200

    LETS FUCKING GOOOOOOO!!! 🔥🔥
    🔥🤩🔥🤩🔥
    - version 2 with new functionalities like multiple folder displaying

--- src/comps/TinderParent.jsx
+++ src/comps/TinderParent.jsx
@@ -1,42 +1,69 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import Tinder from './Tinder';
+import Tinder from "./Tinder";
 
 const TinderParent = ({ map }) => {
     const [currentKeyIndex, setCurrentKeyIndex] = useState(0);
-    const keys = Object.keys(map);
+    const outerFolder = Array.from(map.keys())[0]; // Assuming there's always at least one key, "gui_data" in your case
+    const keys = Array.from(map.get(outerFolder).keys()); // Fetch the subfolder keys (001, 002, 003)
 
     const handleNextKey = () => {
         setCurrentKeyIndex((prevIndex) => (prevIndex + 1) % keys.length);
+        getFilesForCurrentKey();
     };
 
     const handlePrevKey = () => {
         setCurrentKeyIndex((prevIndex) => (prevIndex - 1 + keys.length) % keys.length);
+        getFilesForCurrentKey();
     };
 
     const getCurrentKey = () => {
         return keys[currentKeyIndex];
     };
 
+    const getFilesForCurrentKey = () => {
+        const currentKey = getCurrentKey();
+        const pngFiles = [];
+        const jsonFiles = [];
+
+        const subfolderMap = map.get(outerFolder).get(currentKey);
+        if (subfolderMap instanceof Array) {
+            const allFiles = subfolderMap;
+
+            allFiles.forEach(file => {
+                // Using MIME types instead of file extensions
+                if (file.type === 'image/png') {
+                    pngFiles.push(file);
+                } else if (file.type === 'application/json') {
+                    jsonFiles.push(file);
+                }
+            });
+        }
+
+        console.log("get json files" ,jsonFiles.length);
+        console.log("get png files" ,pngFiles.length);
+
+        return [pngFiles, jsonFiles];
+    };
+
+
+    const [pngFiles, jsonFiles] = getFilesForCurrentKey();
+
     return (
         <>
             <button onClick={handlePrevKey}>&lt;</button>
             <button onClick={handleNextKey}>&gt;</button>
+            <h2>{getCurrentKey()}</h2>
             <Tinder
-                pngFiles={map[getCurrentKey()]}
-                jsonFiles={map[getCurrentKey()]}
+                pngFiles={pngFiles}
+                jsonFiles={jsonFiles}
             />
         </>
     );
 };
 
 TinderParent.propTypes = {
-    map: PropTypes.object.isRequired, // Modify the prop type as per your specific map structure
+    map: PropTypes.object.isRequired,
 };
 
 export default TinderParent;
-
-
-
-
-
